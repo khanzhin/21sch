189b0b31a50b82233a66a79e6c08dd6124cb38ca
Westerling Todd <wtodd@at-r8.21-school.ru>
2019-12-08 11:08:26 +0300

    docker_add

---
+++ Get_next_line/get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wtodd <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/30 11:05:46 by wtodd             #+#    #+#             */
+/*   Updated: 2019/10/22 16:58:02 by wtodd            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include "libft/libft.h"
+# define BUFF_SIZE 100
+
+int							get_next_line(const int fd, char **line);
+
+#endif
---
+++ docker/01_dockerfiles/ex01/team_speak_run.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+
+docker rm -f wtodd > /dev/null
+
+docker build . > .for_run_docker
+
+docker run --name wtodd -dit -p 9987:9987/udp -p 30033:30033/tcp -p 10011:10011 $(cat .for_run_docker | grep "Successfully built" | awk '{print$3}')
+
+sleep 7
+
+docker logs wtodd | grep "token" | tail -n 1 | awk '{print$1}' | pbcopy
+
+rm .for_run_docker
